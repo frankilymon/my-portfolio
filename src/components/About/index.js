e6c7c98810a5f4c23b88a47e70bf9fd7d860d180
Franklin Ukaegbu <franklin.ukaegbu@intrado.com>
2024-01-07 21:17:51 -0500

    updated contact me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,90 +1,83 @@
 import './index.scss'
-import Loader from 'react-loaders';
-import Animation from '../Animation';
-import { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
-const About=()=>{
+import Loader from 'react-loaders'
+import Animation from '../Animation'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    const [letterClass, setLetterClass] = useState('text-animate')
-
-    useEffect(()=> {
-        setTimeout(() =>{
-         setLetterClass('text-animate-hover')
-       }, 3000)
-    }, [])
-return (
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+  return (
     <>
-    <div className="container about-page">
+      <div className="container about-page">
         <div className="text-zone">
-            <h1>
-                <Animation
-                letterClass={letterClass}
-                strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                index={15}
-                />
-            </h1>
-            <p>As an exceptionally motivated front-end developer, 
-                I'm searching for a position in an IT company where 
-                I can use modern technology to work on engaging and diverse projects.
-
-            </p>
-            <p>I'm self-assured, inherently curious and perpetually 
-                honing my design problem-solving abilities.
-
-            </p>
-            <p>
-            I'm highly adaptable, detail-oriented, and communicative. 
-            I consider myself to be a good team player.
-             Although working alone is not a problem for me, I much rather collaborate on projects.
-            </p>
-            <p>
-            I constantly look for fresh, independent ways to advance in my position. 
-            The purpose of these possibilities is to enhance my level of professional competency. 
-            One way to do this might be to attend a symposium, begin a new project, or undergo training.
-            </p>
-            
+          <h1>
+            <Animation
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              index={15}
+            />
+          </h1>
+          <p>
+            As an exceptionally motivated front-end developer, I'm searching for
+            a position in an IT company where I can use modern technology to
+            work on engaging and diverse projects.
+          </p>
+          <p>
+            I'm self-assured, inherently curious and perpetually honing my
+            design problem-solving abilities.
+          </p>
+          <p>
+            I'm highly adaptable, detail-oriented, and communicative. I consider
+            myself to be a good team player. Although working alone is not a
+            problem for me, I much rather collaborate on projects.
+          </p>
+          <p>
+            I constantly look for fresh, independent ways to advance in my
+            position. The purpose of these possibilities is to enhance my level
+            of professional competency. One way to do this might be to attend a
+            symposium, begin a new project, or undergo training.
+          </p>
         </div>
-    <div className='stage-cube-cont'>
-        <div className='cubespinner'>
-            <div className='face1'>
-                <FontAwesomeIcon icon={faAngular} color='#dd0031'/>
-                
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#dd0031" />
             </div>
-            <div className='face2'>
-                <FontAwesomeIcon icon={faHtml5} color='#F06529'/>
-                
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
-            <div className='face3'>
-                <FontAwesomeIcon icon={faCss3} color='#28A4D9'/>
-                
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
             </div>
-            <div className='face4'>
-                <FontAwesomeIcon icon={faReact} color='#5ED4F4'/>
-                
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
             </div>
-            <div className='face5'>
-                <FontAwesomeIcon icon={faJsSquare} color='#EFD81D'/>
-                
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
             </div>
-            <div className='face6'>
-                <FontAwesomeIcon icon={faGitAlt} color='#EC4D28'/>
-                
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
             </div>
-
+          </div>
         </div>
+      </div>
 
-    </div>
-    </div>
-
-<Loader type="pacman"/>
-
-
-
+      <Loader type="pacman" />
     </>
-)
+  )
+}
 
-
-    }
-
-export default About;
+export default About
---
+++ src/components/Contact/index.js
@@ -0,0 +1,119 @@
+import './index.scss'
+import { Loader } from 'react-loaders'
+import Animation from '../Animation'
+import { useState, useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_0rdvbf8',
+        'template_i71y6yw',
+        e.target,
+        'P-Wg98_NSRjvfNcgK'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+    e.target.reset()
+  }
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <Animation
+              letterClass={letterClass}
+              strArray={['c', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              index={15}
+            />
+          </h1>
+          <p>
+            As an enthusiastic IT hobbyist with a strong desire to pursue
+            opportunities for freelancing and a shown ability to manage a
+            variety of projects effectively. Always willing to answer questions
+            and offer assistance as needed. Feel free to reach out for
+            collaboration or consultation.
+          </p>
+          <div className="contact-form">
+            <form onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Franklin Ukaegbu,
+          <br />
+          Canada,
+          <br />
+          Queen-Mary, Snowdon <br />
+          Montreal <br />
+          <span>frankukaegbu04@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer
+            center={[45.48564, -73.62827]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[45.48564, -73.62827]}>
+              <Popup>Franklin lives here, come over for cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
